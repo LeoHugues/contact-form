291d6b952756d49a16267be29dec4278627f8153
LeoHugues <leo.hugues@hotmail.fr>
2021-09-18 15:02:29 +0200

    Add contact form system

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ContactFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(ContactFormType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $contactRequest = $form->getData();
+            $user = $contactRequest->getContactUser();
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $knowUser = $entityManager->getRepository(User::class)->findOneBy(["email" => $user->getEmail()]);
+
+            // If is first contact request from user
+            if(empty($knowUser)) {
+                $user->setPassword('pass');
+                $entityManager->persist($user);
+            } else {
+                $user = $knowUser;
+            }
+
+            $contactRequest->setContactUser($user);
+            $entityManager->persist($contactRequest);
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Nous avons bien reçu votre demande de contact, nous y répondrons dans les plus bref délais');
+        }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'form' => $form->createView(),
         ]);
     }
 }
